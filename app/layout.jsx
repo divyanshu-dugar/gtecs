a4e7f109054830623a8001f66f1d2ab3ef47389d
Divyanshu Dugar <divyu2004@gmail.com>
2025-11-17 12:30:09 -0500

    updated extensiona and removed br

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar /> <br/>
+        <Navbar />
         {children}
         <Footer />
       </body>
